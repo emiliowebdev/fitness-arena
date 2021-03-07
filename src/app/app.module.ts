ddb317997a53b903eefa65f36b1e6b6dddd4f8b9
emiliowebdev <emilio.nero3@gmail.com>
2021-03-07 11:21:42 +0100

    Added bg and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+  EquipmentComponent
+],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EquipmentComponent]
 })
 export class AppModule { }
